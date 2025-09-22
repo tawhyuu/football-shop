c62934d73bec495ed3874489b10a2889a4b20355
Wahyuga <tawahyuga17@gmail.com>
2025-09-22 12:19:18 +0700

    Implement employee management and user authentication features
    - Add Employee model with fields for name, age, and persona.
    - Update Product model to include a ForeignKey to the User model.
    - Create views for adding employees, user registration, login, and logout.
    - Add corresponding templates for employee display, registration, and login.
    - Update main template to display user information and add navigation for product filtering.

--- main/models.py
+++ main/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 import uuid
+from django.contrib.auth.models import User
+
 # Create your models here.
 class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Pemilik Produk", null=True)
     CATEGORY_CHOICES = [
         ('jersey', 'Jersey'),
         ('sepatu', 'Sepatu'),
@@ -24,3 +27,8 @@ class Product(models.Model):
         
     def __str__(self):
         return self.name
+    
+class Employee(models.Model):
+    name = models.CharField(max_length=255)
+    age = models.IntegerField()
+    persona = models.TextField()
--- main/views.py
+++ main/views.py
@@ -1,20 +1,27 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from main.forms import ProductForm
-from main.models import Product
-from django.http import HttpResponse
+from main.models import Product, Employee
+from django.http import HttpResponse, HttpResponseRedirect
 from django.core import serializers
+from django.urls import reverse
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+import datetime
 
 # Create your views here.
+@login_required(login_url='login/')
 def show_main(request):
-    product_list = Product.objects.all()
+    filter_type = request.GET.get('filter', 'all')
+    if filter_type == 'all':
+        product_list = Product.objects.all()
+    else:
+        product_list = Product.objects.filter(user=request.user)
 
     context = {
-        'category' : 'jersey',
-        'name' : 'Jersey Custom',
-        'price' : 150000,
-        'description' : 'Jersey Custom Nama dan Nomor Punggung. Terbuat dari bahan berkualitas tinggi dengan teknologi dry-fit untuk kenyamanan maksimal.',
-        'thumbnail' : 'https://5.imimg.com/data5/SELLER/Default/2024/3/404714376/NM/AG/BO/14081053/football-uniforms.jpg',
-        'is_featured' : True,
+        'name': request.user.username,
+        'last_login': request.COOKIES.get('last_login'),
         'product_list': product_list,
     }
     return render(request, 'main.html', context)
@@ -22,12 +29,24 @@ def show_main(request):
 def create_product(request):
     form = ProductForm(request.POST or None)
     if form.is_valid() and request.method == "POST":
-        form.save()
+        product_entry = form.save(commit=False)
+        product_entry.user = request.user
+        product_entry.save()
         return redirect('main:show_main')
     
     context = {'form': form,}
     return render(request, "create_product.html", context)
 
+def add_employee(request):
+    employee = Employee.objects.create(name = 'Daniel', age = 20, persona = 'baik banget buat challenge')
+    context = {
+        'name' : employee.name,
+        'age' : employee.age,
+        'persona' : employee.persona,
+    }
+    return render(request, "employee.html", context)
+
+@login_required(login_url='login/')
 def show_product(request, id):
     product = get_object_or_404(Product, pk=id)
     context = {
@@ -60,3 +79,36 @@ def show_json_by_id(request, product_id):
         return HttpResponse(json_data, content_type="application/json")
     except Product.DoesNotExist:
         return HttpResponse(status=404)
+    
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+    else:
+        form = AuthenticationForm(request)
+        context = {'form':form}
+    return render(request, 'login.html', context)
+    
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return redirect('main:login')
